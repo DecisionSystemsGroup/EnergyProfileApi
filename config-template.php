c65176ae8fc20dcfe8df59537f3bb704d3ff60f1
alexandros-roumeliotis <r.alex.gr1@gmail.com>
2017-06-23 16:35:20 +0300

    add config-template

---
+++ config-template.php
@@ -0,0 +1,12 @@
+<?php
+	return array(
+		"displayErrorDetails" => true,
+		"addContentLengthHeader" => false,
+		"db" => array(
+			"host" => "",
+			"user" => "",
+			"pass" => "",
+			"dbname" => ""
+		)
+	);
+?>
